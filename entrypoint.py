02687625c6cab56639acc517c27326ef855f19bb
marvin1309 <marvin@fam-feser.de>
2026-07-06 03:15:48 +0200

    chore(release): v1.1.0

--- entrypoint.py
+++ entrypoint.py
@@ -18,7 +18,7 @@ from .app.api import build_plugin_router
 manifest = ModuleManifest(
     id="lyndrix.plugin.bingo",
     name="Meeting Bingo",
-    version="1.0.0",
+    version="1.1.0",
     description="Multiplayer Bullshit-Bingo für langatmige Meetings.",
     author="Lyndrix",
     icon="grid_on",
